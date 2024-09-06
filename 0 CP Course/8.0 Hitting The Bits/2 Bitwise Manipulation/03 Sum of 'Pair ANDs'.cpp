9db1c3f9c0f5445a3c15d85fd0ab3db692ef4465
mamun-akand <mamun.crick25@gmail.com>
2024-09-07 04:51:46 +0600

    Programming

---
+++ 0 CP Course/8.0 Hitting The Bits/2 Bitwise Manipulation/03 Sum of 'Pair ANDs'.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+   ll n; cin>>n; ll ara[n+1];
+   vector<ll> cnt_1(30, 0);
+   for(ll i=1; i<=n; i++){
+      cin>>ara[i];
+      for(ll j=0; j<30; j++){
+         if((ara[i] >> j) & 1) cnt_1[j]++;
+      }
+   }
+   ll sum=0;
+   for(ll k=0; k<30; k++){
+      ll contrib = (cnt_1[k] * cnt_1[k]); 
+      sum += contrib * (1<<k); //1<<k = pow(2, i);
+   }cout << sum << '\n';
+}
--- 0 CP Course/8.0 Hitting The Bits/2 Bitwise Manipulation/04 Sum of 'Pair ORs'.cpp
+++ 0 CP Course/8.0 Hitting The Bits/2 Bitwise Manipulation/04 Sum of 'Pair ORs'.cpp
@@ -5,19 +5,22 @@ using namespace std;
 int main(){
 	ll n; cin>>n;
 	ll ara[n+1];
-	vector<ll> cnt_1(30, 0);
+	vector<ll> cnt_0(30, 0), cnt_1(30, 0);
 	for(ll i=1; i<=n; i++){
 		cin>>ara[i];
 		for(ll j=0; j<30; j++){
 			if((ara[i] >> j) & 1){
 				cnt_1[j]++;
+			}else{
+				cnt_0[j]++;
 			}
 		}
 	}
 	ll sum=0;
 	for(ll k=0; k<30; k++){
-		ll contrib = (cnt_1[k] * cnt_1[k]); 
-		sum += contrib * (1<<k); //1<<k = pow(2, i);
+		ll contrib1 = cnt_1[k] * cnt_1[k]; 
+		ll contrib2 = 2 * cnt_0[k] * cnt_1[k]; 
+		sum += (contrib1+contrib2) * (1<<k); //1<<k = pow(2, i);
 	}cout << sum << '\n';
 }
 
