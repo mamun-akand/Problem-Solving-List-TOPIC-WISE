b04ab005a77af875ed26d01af61c15c3103b67be
mamun-akand <mamun.crick25@gmail.com>
2024-09-25 18:11:32 +0600

    Programming

---
+++ 0 CP Course/9.0 Fermat's Little Secret/0 Problem Solve/05 maximum possible mod over all pair possible.cpp
@@ -0,0 +1,47 @@
+// Find the maximum possible value of a[i] % a[j] over all pairs of i and j
+#include<bits/stdc++.h>
+using namespace std;
+#define   ll    long long int
+
+int main(){
+	ll n; cin>>n;
+	vector<int> v(n); for(auto &it:v) cin>>it;
+	
+	ll first, second;
+	first = second = INT_MIN;
+	
+	for(ll i=0; i<n; i++){
+		if(v[i] > first){
+			second = first;
+			first = v[i];
+		}
+		else if(v[i] > second && v[i] < first){
+			second = v[i];
+		}
+	}
+	
+	if(n < 2 or second == INT_MIN) cout << 0 << endl;
+	else cout << second << endl;
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 0 CP Course/9.0 Fermat's Little Secret/04 Modular Multiplicative Inverse/01 Mulitplicative Inverse with Fermat's Little Theorem.cpp
+++ 0 CP Course/9.0 Fermat's Little Secret/04 Modular Multiplicative Inverse/01 Mulitplicative Inverse with Fermat's Little Theorem.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll = long long;
 //Fermat's Little Theorem
 //a^(p-1) % p = 1; where p = prime
+
 //a^(p-2) % p = 1/a; dividing both side with a, so got inverse
 
 ll binary_exp(ll x, ll n, ll mod){
@@ -17,14 +18,14 @@ ll binary_exp(ll x, ll n, ll mod){
 	} return ans;
 }
 
-ll inverse(ll a, ll m){
-	return binary_exp(a, m-2, m);
+ll multiplicative_inverse(ll a, ll m){
+	return binary_exp(a, m-2, m); //a^(m-2)%m = 1/a;
 }
 
 int main(){
 	ll mod = 1e9+7;
 	ll a; cin>>a;
-	cout << inverse(a, mod) << endl;	
+	cout << multiplicative_inverse(a, mod) << endl;	
 }
 
 
